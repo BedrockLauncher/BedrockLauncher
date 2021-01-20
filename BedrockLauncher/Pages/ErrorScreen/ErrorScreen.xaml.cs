9db5d50ddfb30ed5d2ba4082fe3c4b82cacf0a0e
XlynxX <kirja0821@gmail.com>
2021-01-20 15:10:19 +0200

    added error screen

---
+++ BedrockLauncher/Pages/ErrorScreen/ErrorScreen.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BedrockLauncher
+{
+    /// <summary>
+    /// Логика взаимодействия для ErrorScreen.xaml
+    /// </summary>
+    public partial class ErrorScreen : Page
+    {
+        public ErrorScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void ErrorScreenCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)Application.Current.MainWindow).MainWindowOverlayFrame.Navigate(null);
+        }
+
+        private void ErrorScreenViewCrashButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
