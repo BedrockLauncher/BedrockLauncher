1e6a74f0b69e174379177e2bc434b2c277aa3d68
CarJem Generations <Cwallacecs@Gmail.com>
2021-05-09 21:38:55 -0400

    Final Commits before 0.0.0.9 Beta

--- modules/BL_Core/Properties/Settings.Designer.cs
+++ modules/BL_Core/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace BL_Core.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.0.0.8")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.0.0.9")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
