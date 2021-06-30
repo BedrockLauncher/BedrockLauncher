3908d4fa83dba656c7e44d203f11c52f4f5eeac1
CarJem Generations (Carter Wallace) <cwallacecs@gmail.com>
2021-06-30 14:51:43 -0400

    Bumped to Version 0.0.2.1

--- modules/BedrockLauncher.Core/Properties/Settings.Designer.cs
+++ modules/BedrockLauncher.Core/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace BedrockLauncher.Core.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.0.2.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.0.2.1")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
