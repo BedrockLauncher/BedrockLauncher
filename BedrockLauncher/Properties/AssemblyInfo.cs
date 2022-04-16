373eb2c2d82b3961bf32c48e80303a1f4b65253a
CarJem Generations (Carter Wallace) <cwallacecs@gmail.com>
2022-04-16 18:54:07 -0400

    Language Updates

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2022.4.16.35")]
+[assembly: AssemblyVersion("2022.4.16.37")]
 [assembly: NeutralResourcesLanguage("en-US")]
