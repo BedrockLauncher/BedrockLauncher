89e35646ab3797b6e30a0faad92540ec39f4efe0
CarJem Generations (Carter Wallace) <cwallacecs@gmail.com>
2022-04-18 10:31:12 -0400

    Tweak to SaveRedirectionFailedException to classify it as a PackageManagerException

--- BedrockLauncher/Exceptions/SaveRedirectionFailedException.cs
+++ BedrockLauncher/Exceptions/SaveRedirectionFailedException.cs
@@ -2,7 +2,7 @@
 
 namespace BedrockLauncher.Exceptions
 {
-    public class SaveRedirectionFailedException : Exception
+    public class SaveRedirectionFailedException : PackageManagerException
     {
         public SaveRedirectionFailedException(string message, Exception innerException) : base(message, innerException) { }
         public SaveRedirectionFailedException(Exception innerException) : base(innerException.Message, innerException) { }
