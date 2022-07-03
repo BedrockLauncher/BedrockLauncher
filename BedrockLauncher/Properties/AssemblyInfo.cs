fd62b51344c76c59ce6db2f62cdf1c6b44634b52
CarJem Generations (Carter Wallace) <cwallacecs@gmail.com>
2022-07-03 18:06:09 -0400

    Final About Page Update

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2022.7.3.54")]
+[assembly: AssemblyVersion("2022.7.3.56")]
 [assembly: NeutralResourcesLanguage("en-US")]
