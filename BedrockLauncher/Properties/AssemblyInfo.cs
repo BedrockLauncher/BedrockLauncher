66a3cd817f6237b0236cf78f5922f0ed6380edfe
CarJem Generations (Carter Wallace) <cwallacecs@gmail.com>
2022-08-23 16:31:17 -0400

    Remove NET Standard Requirement

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2022.7.7.183")]
+[assembly: AssemblyVersion("2022.8.23.74")]
 [assembly: NeutralResourcesLanguage("en-US")]
