55229bdc278ef92d7b0115941d66d07d7276abfe
Ryan Morrison <Rayth@users.noreply.github.com>
2023-03-13 21:12:13 +0000

    Revert AssemblyInfo.cs

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -21,7 +21,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-
 [assembly: AssemblyVersion("2023.3.12.2")]
-[assembly: AssemblyFileVersion("2023.3.12.2")]
 [assembly: NeutralResourcesLanguage("en-US")]
