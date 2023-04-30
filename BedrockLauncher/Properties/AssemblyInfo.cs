077f284f78f54c875801d3b2ee63065a41d9286d
CarJem Generations <Cwallacecs@Gmail.com>
2023-04-30 14:31:27 -0700

    Merged BedrockLauncher.Backend with the main project

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2023.4.30.40")]
+[assembly: AssemblyVersion("2023.4.30.91")]
 [assembly: NeutralResourcesLanguage("en-US")]
