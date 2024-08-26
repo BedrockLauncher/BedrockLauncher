0eeb6a0939069674179838098c7b070d620f4285
Ryan Morrison <Rayth@users.noreply.github.com>
2024-08-26 12:06:53 +0100

    Update AssemblyInfo.cs

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2024.8.26.7")]
+[assembly: AssemblyVersion("2024.8.26.16")]
 [assembly: NeutralResourcesLanguage("en-US")]
