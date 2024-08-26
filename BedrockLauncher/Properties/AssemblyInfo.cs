631bb0477067a788fbcb32ac7257b51db8622251
Ryan Morrison <Rayth@users.noreply.github.com>
2024-08-26 11:34:34 +0100

    Fix to #505

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2024.8.18.46")]
+[assembly: AssemblyVersion("2024.8.26.6")]
 [assembly: NeutralResourcesLanguage("en-US")]
