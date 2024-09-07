b4370f7bf0a7e7bea2a472e76344da2725cbe534
Ryan Morrison <Rayth@users.noreply.github.com>
2024-09-07 21:35:31 +0100

    Fix auto restart after clicking apply and restart button.
    Fixes #510

--- BedrockLauncher/Properties/AssemblyInfo.cs
+++ BedrockLauncher/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 
-[assembly: AssemblyVersion("2024.8.26.28")]
+[assembly: AssemblyVersion("2024.9.7.6")]
 [assembly: NeutralResourcesLanguage("en-US")]
